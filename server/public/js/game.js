1dbb2f2e55db5934e70f87b4015518084f376696
Matt Stockmal <mestockmal@gmail.com>
2019-04-17 14:13:08 -0700

    Add Game V.1

---
+++ server/public/js/game.js
@@ -0,0 +1,114 @@
+var config = {
+  type: Phaser.AUTO,
+  parent: 'phaser-example',
+  width: 1280,
+  height: 800,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('lamp', 'assets/PNM_Lamp.png');
+  this.load.image('otherPlayer', 'assets/enemyBlack5.png');
+  this.load.image('star', 'assets/PNM_Star.png');
+  this.load.image('sky', 'assets/PNM_Background.jpg');
+}
+
+function create() {
+  var self = this;
+  this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  this.socket = io();
+  this.players = this.add.group();
+
+  this.blueScoreText = this.add.text(16, 16, '', { fontSize: '32px', fill: '#05fcfc' });
+  this.redScoreText = this.add.text(584, 16, '', { fontSize: '32px', fill: '#ff26a1' });
+
+  this.socket.on('currentPlayers', function (players) {
+    Object.keys(players).forEach(function (id) {
+      if (players[id].playerId === self.socket.id) {
+        displayPlayers(self, players[id], 'lamp');
+      } else {
+        displayPlayers(self, players[id], 'otherPlayer');
+      }
+    });
+  });
+
+  this.socket.on('newPlayer', function (playerInfo) {
+    displayPlayers(self, playerInfo, 'otherPlayer');
+  });
+
+  this.socket.on('disconnect', function (playerId) {
+    self.players.getChildren().forEach(function (player) {
+      if (playerId === player.playerId) {
+        player.destroy();
+      }
+    });
+  });
+
+  this.socket.on('playerUpdates', function (players) {
+    Object.keys(players).forEach(function (id) {
+      self.players.getChildren().forEach(function (player) {
+        if (players[id].playerId === player.playerId) {
+          player.setRotation(players[id].rotation);
+          player.setPosition(players[id].x, players[id].y);
+        }
+      });
+    });
+  });
+
+  this.socket.on('updateScore', function (scores) {
+    self.blueScoreText.setText('Teal: ' + scores.blue);
+    self.redScoreText.setText('Pink: ' + scores.red);
+  });
+
+  this.socket.on('starLocation', function (starLocation) {
+    if (!self.star) {
+      self.star = self.add.image(starLocation.x, starLocation.y, 'star').setDisplaySize(60, 60);
+    } else {
+      self.star.setPosition(starLocation.x, starLocation.y);
+    }
+  });
+
+  this.cursors = this.input.keyboard.createCursorKeys();
+  this.leftKeyPressed = false;
+  this.rightKeyPressed = false;
+  this.upKeyPressed = false;
+}
+
+function update() {
+  const left = this.leftKeyPressed;
+  const right = this.rightKeyPressed;
+  const up = this.upKeyPressed;
+
+  if (this.cursors.left.isDown) {
+    this.leftKeyPressed = true;
+  } else if (this.cursors.right.isDown) {
+    this.rightKeyPressed = true;
+  } else {
+    this.leftKeyPressed = false;
+    this.rightKeyPressed = false;
+  }
+
+  if (this.cursors.up.isDown) {
+    this.upKeyPressed = true;
+  } else {
+    this.upKeyPressed = false;
+  }
+
+  if (left !== this.leftKeyPressed || right !== this.rightKeyPressed || up !== this.upKeyPressed) {
+    this.socket.emit('playerInput', { left: this.leftKeyPressed , right: this.rightKeyPressed, up: this.upKeyPressed });
+  }
+}
+
+function displayPlayers(self, playerInfo, sprite) {
+  const player = self.add.sprite(playerInfo.x, playerInfo.y, sprite).setOrigin(0.5, 0.5).setDisplaySize(83, 70);
+  if (playerInfo.team === 'blue') player.setTint(0x05fcfc);
+  else player.setTint(0xff26a1);
+  player.playerId = playerInfo.playerId;
+  self.players.add(player);
+}
